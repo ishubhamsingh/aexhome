2874e7085fd88fdd069b30ceb57e22102b442bba
sb6596 <shubhamprince111@gmail.com>
2022-02-07 08:22:53 +0000

    aexhome: Update for Twelve
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>
    Change-Id: Idc3b59da4d534a4b60ce3d864f0624693ac37bd2

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,5 +1,5 @@
 export const downloadCenterUrl = "https://downloads.aospextended.com";
 export const statsUrl = "https://stats.aospextended.com";
-export const legacyAPI = "nougat";
+export const legacyAPI = "Ten";
 export const ApiBaseUrl = "https://api.aospextended.com";
 export const GA_ID = "UA-89927166-2";
